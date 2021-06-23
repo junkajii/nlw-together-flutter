75d644afcd77275f9657d1029d601fa80929c431
Jun Kajii <junk4jii@gmail.com>
2021-06-23 16:27:06 -0300

    🎨 update pages

--- lib/modules/login/login_page.dart
+++ lib/modules/login/login_page.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:payflow/modules/login/login_controller.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 import 'package:payflow/shared/themes/app_images.dart';
 import 'package:payflow/shared/themes/app_text_styles.dart';
@@ -14,6 +15,8 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final controller = LoginController();
+
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
@@ -45,13 +48,13 @@ class _LoginPageState extends State<LoginPage> {
               ],
             ),
           ),
-          SizedBox(
-            height: 24,
-          ),
+          // SizedBox(
+          //   height: 24,
+          // ),
           Image.asset(AppImages.logomini),
-          SizedBox(
-            height: 24,
-          ),
+          // SizedBox(
+          //   height: 24,
+          // ),
           Container(
             width: 250,
             child: Text(
@@ -60,11 +63,13 @@ class _LoginPageState extends State<LoginPage> {
               textAlign: TextAlign.center,
             ),
           ),
-          SizedBox(
-            height: 40,
-          ),
+          // SizedBox(
+          //   height: 40,
+          // ),
           SocialLoginButton(
-            onTap: () {},
+            onTap: () {
+              controller.googleSignIn(context);
+            },
           ),
         ],
       ),
--- lib/modules/splash/splash_page.dart
+++ lib/modules/splash/splash_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:payflow/shared/auth/auth_controller.dart';
 import 'package:payflow/shared/themes/app_images.dart';
 
 class SplashPage extends StatelessWidget {
@@ -6,6 +7,9 @@ class SplashPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final authController = AuthController();
+    authController.currentUser(context);
+
     return Scaffold(
       body: Center(
         child: Stack(
