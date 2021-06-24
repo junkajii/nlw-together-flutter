106f265dbffbe30fe288113837427b080223870a
Jun Kajii <junk4jii@gmail.com>
2021-06-24 10:39:18 -0300

    ♻️ refactoring code

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:payflow/modules/home/home_page.dart';
+import 'package:flutter/services.dart';
 
+import 'modules/home/home_page.dart';
+import 'modules/insert_boleto/insert_boleto_page.dart';
+import 'modules/barcode_scanner/barcode_scanner_page.dart';
 import 'modules/login/login_page.dart';
 import 'modules/splash/splash_page.dart';
 import 'shared/themes/app_colors.dart';
 
 class AppWidget extends StatelessWidget {
-  const AppWidget({Key? key}) : super(key: key);
+  AppWidget() {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitDown,
+      DeviceOrientation.portraitUp,
+    ]);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,6 +22,7 @@ class AppWidget extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Pay Flow',
       theme: ThemeData(
+        primarySwatch: Colors.orange,
         primaryColor: AppColors.primary,
       ),
       initialRoute: '/',
@@ -21,6 +30,8 @@ class AppWidget extends StatelessWidget {
         '/': (context) => SplashPage(),
         '/home': (context) => HomePage(),
         '/login': (context) => LoginPage(),
+        '/barcode_scanner': (context) => BarcodeScannerPage(),
+        '/insert_boleto': (context) => InsertBoletoPage(),
       },
     );
   }
