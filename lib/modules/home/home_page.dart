79caca94edcd93d53fe4754b200bab76578552bb
Jun Kajii <junk4jii@gmail.com>
2021-06-26 10:28:42 -0300

    🐛 fix

--- lib/modules/home/home_page.dart
+++ lib/modules/home/home_page.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+
+import 'package:payflow/modules/extract/extract_page.dart';
 import 'package:payflow/modules/home/home_controller.dart';
+import 'package:payflow/modules/meus_boletos/meus_boletos_page.dart';
+import 'package:payflow/shared/models/user_model.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 import 'package:payflow/shared/themes/app_text_styles.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
+  final UserModel user;
+
+  const HomePage({
+    Key? key,
+    required this.user,
+  }) : super(key: key);
 
   @override
   _HomePageState createState() => _HomePageState();
@@ -12,14 +21,6 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   final controller = HomeController();
-  final pages = [
-    Container(
-      color: Colors.purple,
-    ),
-    Container(
-      color: Colors.blue,
-    ),
-  ];
 
   @override
   Widget build(BuildContext context) {
@@ -37,7 +38,7 @@ class _HomePageState extends State<HomePage> {
                   style: AppTextStyles.titleRegular,
                   children: [
                     TextSpan(
-                      text: 'Jun Kajii',
+                      text: '${widget.user.name}',
                       style: AppTextStyles.titleBoldBackground,
                     ),
                   ],
@@ -47,19 +48,34 @@ class _HomePageState extends State<HomePage> {
                 'Mantenha suas contas em dia',
                 style: AppTextStyles.captionShape,
               ),
-              trailing: Container(
-                width: 48,
-                height: 48,
-                decoration: BoxDecoration(
-                  color: Colors.black,
-                  borderRadius: BorderRadius.circular(5),
+              trailing: GestureDetector(
+                onTap: () {
+                  print('logout!');
+                },
+                child: Container(
+                  width: 48,
+                  height: 48,
+                  decoration: BoxDecoration(
+                    color: AppColors.background,
+                    borderRadius: BorderRadius.circular(5),
+                    image: DecorationImage(
+                      image: NetworkImage(widget.user.photoURL!),
+                    ),
+                  ),
                 ),
               ),
             ),
           ),
         ),
       ),
-      body: pages[controller.currentPage],
+      body: [
+        MeusBoletosPage(
+          key: UniqueKey(),
+        ),
+        ExtractPage(
+          key: UniqueKey(),
+        ),
+      ][controller.currentPage],
       bottomNavigationBar: Container(
         height: 90,
         child: Row(
@@ -70,14 +86,17 @@ class _HomePageState extends State<HomePage> {
                 controller.setPage(0);
                 setState(() {});
               },
-              color: AppColors.primary,
+              color: controller.currentPage == 0
+                  ? AppColors.primary
+                  : AppColors.body,
               icon: Icon(
                 Icons.home,
               ),
             ),
             GestureDetector(
-              onTap: () {
-                Navigator.pushNamed(context, '/barcode_scanner');
+              onTap: () async {
+                await Navigator.pushNamed(context, '/barcode_scanner');
+                setState(() {});
               },
               child: Container(
                 width: 56,
@@ -97,7 +116,9 @@ class _HomePageState extends State<HomePage> {
                 controller.setPage(1);
                 setState(() {});
               },
-              color: AppColors.body,
+              color: controller.currentPage == 1
+                  ? AppColors.primary
+                  : AppColors.body,
               icon: Icon(
                 Icons.description_outlined,
               ),
