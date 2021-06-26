43836c3cbae065f6bd65a01959b5cb1acc238675
Jun Kajii <junk4jii@gmail.com>
2021-06-26 10:23:25 -0300

    🔥 remove code

--- lib/shared/models/user_model.dart
+++ lib/shared/models/user_model.dart
@@ -27,15 +27,4 @@ class UserModel {
 
   factory UserModel.fromJson(String source) =>
       UserModel.fromMap(json.decode(source));
-
-  // factory UserModel.fromMap(Map<String, dynamic> map) {
-  //   return UserModel(name: map['name'], photoURL: map['photoURL']);
-  // }
-
-  // Map<String, dynamic> toMap() => {
-  //       'name': name,
-  //       'photoURL': photoURL,
-  //     };
-
-  // String toJson() => jsonEncode(toMap());
 }
